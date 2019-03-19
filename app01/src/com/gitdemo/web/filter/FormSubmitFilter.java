40514877afafdbf785fd09ad8aa2c21a2b5a84e4
LZB <linzhibin@hongyangsoft.com>
2019-03-20 00:55:16 +0800

    主干

--- app01/src/com/gitdemo/web/filter/FormSubmitFilter.java
+++ app01/src/com/gitdemo/web/filter/FormSubmitFilter.java
@@ -12,9 +12,9 @@ public class FormSubmitFilter implements Filter {
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
         // 修改bug
-        System.out.println("===========拦截请求-2.0分支开发.===========");
+        System.out.println("===========拦截请求-主干分支===========");
         String username = (String) req.getParameter("username");
-        if (username == null || isVerifycode(req, resp)) {
+        if (username == null) {
             System.out.println("被拦截：跳转到login页面！");
             req.getRequestDispatcher("/login.jsp").forward(req, resp);
         } else {
@@ -27,17 +27,4 @@ public class FormSubmitFilter implements Filter {
     public void init(FilterConfig config) throws ServletException {
 
     }
-
-    public boolean isVerifycode(ServletRequest request, ServletResponse response){
-        String verifycode =request.getParameter("verifycode");
-        if(verifycode.isEmpty()){
-            //验证码不正确
-            //提示信息
-            request.setAttribute("login_msg", "验证码不存在");
-            //跳转登录页面
-            return false;
-        }
-        return true;
-    }
-
 }
